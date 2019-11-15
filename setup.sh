fdc564ab02ae685f0c622cf7b3547114595bf0ab
rooneyshuman <rooneyshuman@gmail.com>
2019-11-14 21:21:38 -0800

    Modified setup script to include adding new vm's ip to allowed hosts list

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+# Update packages and install dev tools
 sudo apt update
 sudo apt install python3-pip python3-dev libpq-dev postgresql git postgresql-contrib nginx curl vim
+
+# Set up postgres database
 sudo -u postgres psql -c "CREATE DATABASE sreproject"
 sudo -u postgres psql -c "CREATE USER sreprojectuser WITH PASSWORD 'sreprojectpw'"
 sudo -u postgres psql -c "ALTER ROLE sreprojectuser SET client_encoding TO 'utf8'"
@@ -10,12 +13,21 @@ sudo -u postgres psql -c "ALTER ROLE sreprojectuser SET timezone TO 'UTC'"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sreproject TO sreprojectuser"
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv
+
+# Activate python dev environment and install remaining tools
 cd ~/django-polling
 source pollingenv/bin/activate
 sudo pip install django gunicorn psycopg2-binary
+
+# Modify allowed hosts to include fresh vm
+myIP=$(curl https://ipinfo.io/ip)
+echo "ALLOWED_HOSTS = ['$myIP', 'localhost', 'bestgroupsre.ipq.co']" >> django_polling/settings.py
+
+# Migrate db and app settings
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
 sudo python3 manage.py collectstatic
-cd ..
+
+# Run gunicorn server
 gunicorn --bind 0.0.0.0:8000 django_polling.wsgi
