e0e2c42a9f270763ee75256404aacd1eb0b954b8
rooneyshuman <rooneyshuman@gmail.com>
2019-11-14 22:31:26 -0800

    Updated nginx setup script to include comments

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo su -
-cp ../home/bbelen/django-polling/pollconfig /etc/nginx/sites-available
+
+# Copy nginx configs
+cp pollconfig /etc/nginx/sites-available
+
+# Create symbolic link
 ln -s /etc/nginx/sites-available/pollconfig /etc/nginx/sites-enabled/pollconfig
-rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+
+# Check nginx configs
 nginx -t
+
+# Restart nginx
 systemctl restart nginx
